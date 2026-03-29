bf16169a5377ff83dc7c804f446a9fc3a71c5b93
ChaZar <chahine.bensalah@etudiant-fst.utm.tn>
2026-03-29 16:53:29 +0100

    Update CreateTransportRequest.java

--- src/main/java/com/tuniway/connect/model/dto/CreateTransportRequest.java
+++ src/main/java/com/tuniway/connect/model/dto/CreateTransportRequest.java
@@ -7,8 +7,9 @@ import com.tuniway.connect.model.entity.TransportType;
 public class CreateTransportRequest {
     private String code;
     private String name;
-    @JsonAlias({"route_name", "routeName"})
-    private String route_name;
+    @JsonProperty("route_name")
+    @JsonAlias("routeName")
+    private String routeName;
     private TransportType transportType;
     private String start_point;
     private String end_point;
@@ -33,16 +34,11 @@ public class CreateTransportRequest {
     }
 
     public String getRouteName() {
-        return route_name;
+        return routeName;
     }
 
-    public void setRouteName(String route_name) {
-        this.route_name = route_name;
-    }
-
-    @JsonProperty("route_name")
-    public void setRoute_name(String route_name) {
-        this.route_name = route_name;
+    public void setRouteName(String routeName) {
+        this.routeName = routeName;
     }
 
     public String getStart_point() {
